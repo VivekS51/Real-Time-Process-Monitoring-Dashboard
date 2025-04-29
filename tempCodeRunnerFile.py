02c368ab0040ab594ef59dff3b5156bb330f9fa9
Vivek Sagar <144837426+VivekS51@users.noreply.github.com>
2025-04-29 16:11:31 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget, QWidget, QVBoxLayout, QSizePolicy
-from cpu_details import CPUMonitorWidget
-from memory_details import MemoryMonitorWidget
-from disk_details import DiskMonitorWidget
-from network_details import NetworkMonitorWidget
-from gpu_details import GPUMonitorWidget
-import pyqtgraph as pg
